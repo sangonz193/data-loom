f3f02fb636f7e97dc960a70dd06b64e090e2cb3a
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-04-20 23:56:38 -0300

    generate db types

--- supabase/types.gen.ts
+++ supabase/types.gen.ts
@@ -34,7 +34,21 @@ export type Database = {
   }
   public: {
     Tables: {
-      [_ in never]: never
+      users: {
+        Row: {
+          created_at: string | null
+          id: string
+        }
+        Insert: {
+          created_at?: string | null
+          id: string
+        }
+        Update: {
+          created_at?: string | null
+          id?: string
+        }
+        Relationships: []
+      }
     }
     Views: {
       [_ in never]: never
@@ -437,4 +451,3 @@ export type Enums<
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
     : never
-
