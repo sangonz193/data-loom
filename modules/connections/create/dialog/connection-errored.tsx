22173c653d9a065c86e689d12e69b300918849b3
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-04-24 23:03:59 -0300

    minor error message change

--- modules/connections/create/dialog/connection-errored.tsx
+++ modules/connections/create/dialog/connection-errored.tsx
@@ -7,14 +7,10 @@ export function ConnectionErrored() {
 
       <div className="gap-2 text-balance text-foreground">
         <span>
-          We couldn’t establish a direct link between the devices. For a better
+          Couldn’t establish a direct link between the devices. For a better
           chance at a successful connection, consider these tips:
         </span>
         <ul className="list-disc space-y-2 pl-6">
-          <li>
-            If possible, connect both devices to the same Wi-Fi network. This
-            often helps but is not required.
-          </li>
           <li>
             Avoid using public Wi-Fi or mobile data, as these networks might
             restrict peer-to-peer connections.
@@ -22,6 +18,10 @@ export function ConnectionErrored() {
           <li>
             Ensure that firewall settings or other network restrictions are not
             blocking the connection.
+          </li>
+          <li>
+            If possible, connect both devices to the same Wi-Fi network. This
+            often helps but is not required.
           </li>
         </ul>
       </div>
