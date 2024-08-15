c09eced44167cafbcfb48db066f4004693db32e6
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-08-15 18:12:14 -0300

    test sameSite header to none

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -16,7 +16,13 @@ export const createClient = () => {
         },
         set(name: string, value: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value, ...options })
+            cookieStore.set({
+              name,
+              value,
+              ...options,
+              sameSite: "none",
+              secure: true,
+            })
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
